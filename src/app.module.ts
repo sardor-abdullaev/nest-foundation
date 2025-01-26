78784cf872d9cf1031bedff402eeaa7701173dfb
Sardor Abdullaev <abdullaevss94@mail.ru>
2025-01-26 23:08:13 +0500

    add logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
+import { SongsController } from './songs/songs.controller';
 
 @Module({
   imports: [SongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    //option 1
+    // consumer.apply(LoggerMiddleware).forRoutes('songs') 
+
+    //option 2
+    // consumer.apply(LoggerMiddleware).forRoutes({ path: 'songs', method: RequestMethod.POST }) 
+
+    // option 3
+    consumer.apply(LoggerMiddleware).forRoutes(SongsController)
+  }
+}
