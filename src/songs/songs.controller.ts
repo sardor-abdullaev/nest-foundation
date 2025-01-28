842c4a772185d5a1190a9340e0944a524d1fffaf
sardor-abdullaev <abdullaevss94@mail.ru>
2025-01-28 16:19:14 +0500

    add scope

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,14 +1,30 @@
-import { Body, Controller, Delete, Get, HttpException, HttpStatus, Inject, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Scope,
+} from '@nestjs/common';
 import { SongsService } from './songs.service';
 import { CreateSongDTO } from './dto/create-song-dto';
 import { Connection } from 'src/common/constants/connection';
 
-@Controller('songs')
+@Controller({ path: 'songs', scope: Scope.REQUEST })
 export class SongsController {
-  constructor(private songsService: SongsService,
-    @Inject('CONNECTION') private connection: Connection
+  constructor(
+    private songsService: SongsService,
+    @Inject('CONNECTION') private connection: Connection,
   ) {
-    console.log(`This is a connetion string: ${this.connection.CONNECTION_STRING}`);
+    console.log(
+      `This is a connetion string: ${this.connection.CONNECTION_STRING}`,
+    );
   }
 
   @Post()
@@ -21,12 +37,22 @@ export class SongsController {
     try {
       return this.songsService.findAll();
     } catch (error) {
-      throw new HttpException('server error', HttpStatus.INTERNAL_SERVER_ERROR, { cause: error })
+      throw new HttpException(
+        'server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        { cause: error },
+      );
     }
   }
 
   @Get(':id')
-  findOne(@Param('id', new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE })) id: number) {
+  findOne(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    id: number,
+  ) {
     return `fetch song on based on the id ${typeof id}`;
   }
 
